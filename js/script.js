f634b8f33b4a1fdf5925680c0ed23ffed12d3cba
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2017-08-12 12:07:20 +0700

    сверстал мобильную версию без модальных окон

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// header-mobile-menu
 var toggleMenu = document.querySelector('.main-header__toggle-menu');
 var iconMenu = document.querySelector('.main-header__toggle-menu--close');
 var mobileMenu = document.querySelector('.main-navigation');
@@ -21,5 +22,32 @@ toggleMenu.addEventListener('click', function(event){
 
     mobileMenu.classList.remove('main-navigation--header-on');
     mobileMenu.classList.add('main-navigation--header-off');
+  }
+});
+
+// footer-mobile-menu
+var footerToggleMenu = document.querySelector('.main-footer__toggle-menu');
+var footerIconMenu = document.querySelector('.main-footer__toggle-menu--close');
+var footerMobileMenu = document.querySelector('.main-navigation--footer');
+
+footerMobileMenu.classList.remove('main-navigation--footer-nojs');
+footerMobileMenu.classList.add('main-navigation--footer-off');
+
+footerToggleMenu.addEventListener('click', function(event){
+  event.preventDefault();
+  if (footerIconMenu.classList.contains('main-footer__toggle-menu--close')) {
+    footerIconMenu.classList.remove('main-footer__toggle-menu--close');
+    footerIconMenu.classList.add('main-footer__toggle-menu--open');
+
+    footerMobileMenu.classList.remove('main-navigation--footer-off');
+    footerMobileMenu.classList.add('main-navigation--footer-on');
+  }
+
+  else {
+    footerIconMenu.classList.remove('main-footer__toggle-menu--open');
+    footerIconMenu.classList.add('main-footer__toggle-menu--close');
+
+    footerMobileMenu.classList.remove('main-navigation--footer-on');
+    footerMobileMenu.classList.add('main-navigation--footer-off');
   }
 });
